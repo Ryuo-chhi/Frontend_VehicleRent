bc2c1c6cda38ad280e731edaccd65df6d005125d
chhi-hiro <chhunhourtiek255@gmail.com>
2026-03-13 12:17:47 +0700

    initial

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { FaBeer } from "react-icons/fa";
+
+const App = () => {
+  return (
+    <div className="bg-amber-700">
+      <h3>
+        Lets go for a <FaBeer />?
+      </h3>
+    </div>
+  );
+};
+
+export default App;
