4c7d78b4f934828548fa2dd7d2a8909a00406877
thangsaoly <lovepapa987@gmail.com>
2026-03-13 15:58:37 +0700

    FeatureCard added

---
+++ src/datas/featureCard.js
@@ -0,0 +1,27 @@
+import { LuCar, LuBadgeCheck, LuHeadset } from "react-icons/lu";
+
+export const featureCardData = [
+  {
+    id: 1,
+    icon: LuCar,
+    bgIcon: "#0D99FF",
+    title: "Wide Selection",
+    description: "Over 500+ premium vehicles to choose from.",
+  },
+  {
+    id: 2,
+    icon: LuBadgeCheck,
+    bgIcon: "#14AE5C",
+    title: "Verify Fleet",
+    description: "Every vehicle is inspected and certified.",
+  },
+  {
+    id: 3,
+    icon: LuHeadset,
+    bgIcon: "#9747FF",
+    title: "24/7 Support",
+    description: "Our team is here to help anytime.",
+  },
+];
+
+export default featureCardData;
