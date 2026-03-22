8bd0abecf0fcc603f64a50c85b8a14179483c77c
Noy Sokbolen <bolen0096@gmail.com>
2026-03-22 23:20:26 +0700

    fix conflict

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
 
 import { Home } from "./pages/Home";
-<<<<<<< Updated upstream
+
 import About from "./pages/About";
-=======
+
 import { Vehicles } from "./pages/Vehicle"; 
->>>>>>> Stashed changes
+
 
 const App = () => {
   return (
     <div className="mx-auto">
       <Home />
-<<<<<<< Updated upstream
+
       <About />
-=======
+
       <Vehicles />
->>>>>>> Stashed changes
+
     </div>
   );
 };
