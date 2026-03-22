7ebde57016fbec8c5bb9c321151c1cf76a975310
Noy Sokbolen <bolen0096@gmail.com>
2026-03-22 23:05:07 +0700

    save current progress before applying stash

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 
 import { Home } from "./pages/Home";
+import About from "./pages/About";
 
 const App = () => {
   return (
     <div className="mx-auto">
       <Home />
+      <About />
     </div>
   );
 };
